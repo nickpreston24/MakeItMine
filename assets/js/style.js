e5b9de03fc9f3b3ff90549ffba6b4199ae864617
daniel 'kirplink' reynolds <=>
2018-11-05 21:36:56 -0600

    adding the menu animation

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,8 +1,14 @@
 $(document).on('click', '#account-icon', function() {
+    $('.menu').addClass('animate-menu-out').removeClass('animate-menu-in');
     $('.menu-container').css('display', 'initial');
     // alert('you clicked!')
-})
+});
 
 $(document).on('click', '#close-menu', function() {
-    $('.menu-container').css('display', 'none');
-})
+    $('.menu').addClass('animate-menu-in').removeClass('animate-menu-out');
+    function hideMenu() {
+        $('.menu-container').css('display', 'none');
+    } 
+    setTimeout(hideMenu, 300)
+});
+
