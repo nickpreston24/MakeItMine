b4d91de6fee44200c67c757117faa41d15d0908c
mikepreston17 <michael.n.preston@gmail.com>
2018-11-07 19:28:27 -0600

    ) fixed

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,24 +1,26 @@
-$(document).on('click', '#account-icon', function() {
+$(document).on('click', '#account-icon', function () {
     $('.menu').addClass('animate-menu-out').removeClass('animate-menu-in');
     $('.menu-container').css('display', 'initial');
+})
 
-$(document).on('click', '#close-menu', function() {
+$(document).on('click', '#close-menu', function () {
     $('.menu').addClass('animate-menu-in').removeClass('animate-menu-out');
+
     function hideMenu() {
         $('.menu-container').css('display', 'none');
-    } 
+    }
     setTimeout(hideMenu, 300)
 });
 
-$(document).on('click', "#login-link", function() {
+$(document).on('click', "#login-link", function () {
     $('#login-form-div').removeClass('display-none');
 })
 
-$(document).on('click', "#signup-link", function() {
+$(document).on('click', "#signup-link", function () {
     $('#signup-form-div').removeClass('display-none');
 })
 
-$(document).on('click', '#back-arrow', function() {
+$(document).on('click', '#back-arrow', function () {
     $("#recipe-info").show();
     $("#search-form").show();
     $("#recipe-view-div").hide();
