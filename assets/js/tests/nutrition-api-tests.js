28a9c87b18e2ed0a117c8ead461ab84b5fd19041
mikepreston17 <michael.n.preston@gmail.com>
2018-11-08 15:29:51 -0600

    renamed nutrition apikey

--- assets/js/tests/nutrition-api-tests.js
+++ assets/js/tests/nutrition-api-tests.js
@@ -1,4 +1,4 @@
-const apiKey = "38d3947a3f2af312047999390586a0ad";
+const nutritionAPIKey = "38d3947a3f2af312047999390586a0ad";
 
 const baseURL = "https://edamam-edamam-nutrition-analysis.p.mashape.com/api/nutrition-data?"
 //sample: ingr=1+large+apple
